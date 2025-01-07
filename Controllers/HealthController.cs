4b20ef8a60b46224e4458322966c5161cec4fc3b
Agnes <133767448+agnes-vj@users.noreply.github.com>
2025-01-07 10:47:31 +0000

    Add health check

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConferenceManager.Controllers
+{
+    [ApiController]
+    [Route("/api/[Controller]")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public IActionResult getHealth()
+        {
+            return Ok("Welcome!");
+        }
+    }
+}
